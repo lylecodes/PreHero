a5c7eafc363961a3c93b7bf16672cf05bc2b1253
lylej312 <lyle.jensen95@gmail.com>
2021-08-22 01:35:15 -0400

    bug fixes

--- components/create/DropdownMenuContainer.js
+++ components/create/DropdownMenuContainer.js
@@ -1,16 +1,12 @@
 import NavItem from './NavItem.js'
 import Navbar from './Navbar.js';
 import DropdownMenu from './DropdownMenu.js';
+import classes from './DropdownMenu.module.css'
 
 const DropdownMenuContainer = () => {
     return (
-        <div>
-            <Navbar>
-                
-            <NavItem>
+        <div className={classes.dropdownContainer}>
                 <DropdownMenu />
-            </NavItem>
-            </Navbar>
           </div>
     )
 }

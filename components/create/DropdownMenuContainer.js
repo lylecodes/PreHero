7dac16dfe26af2a4edf555392cf5244f1ae0f602
Lyle Jensen <74841752+lylej312@users.noreply.github.com>
2021-08-23 22:39:59 -0400

    refactored dropdown menu

--- components/create/DropdownMenuContainer.js
+++ components/create/DropdownMenuContainer.js
@@ -1,14 +1,14 @@
-import NavItem from './NavItem.js'
-import Navbar from './Navbar.js';
-import DropdownMenu from './DropdownMenu.js';
-import classes from './DropdownMenu.module.css'
+import NavItem from "./NavItem.js";
+import Navbar from "./Navbar.js";
+import DropdownMenu from "./DropdownMenu.js";
+import classes from "./DropdownMenu.module.css";
 
 const DropdownMenuContainer = () => {
-    return (
-        <div className={classes.dropdownContainer}>
-                <DropdownMenu />
-          </div>
-    )
-}
+  return (
+    <div className={classes.dropdownContainer}>
+      <DropdownMenu />
+    </div>
+  );
+};
 
 export default DropdownMenuContainer;

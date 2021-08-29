614aaa121331c14b51e1f8e9e727201ac2d775b2
Lyle Jensen <74841752+lylej312@users.noreply.github.com>
2021-08-29 14:00:59 -0400

    fixed css bugs

--- components/create/IngredientDropdownMenu/MenuItem.js
+++ components/create/IngredientDropdownMenu/MenuItem.js
@@ -5,11 +5,12 @@ import classes from "./IngredientDropdownMenu.module.css";
 const MenuItem = (props) => {
   const displayedName = props.category || props.name;
 
+  const sendMenuHandler = () => {
+    props.sendMenu(displayedName.toLowerCase());
+  };
+
   return (
-    <li
-      className={classes.menuItem}
-      onClick={() => props.sendMenu(displayedName.toLowerCase())}
-    >
+    <li className={classes.menuItem} onClick={sendMenuHandler}>
       <span className={classes.iconButton}>{props.leftIcon}</span>
       <h2>{displayedName}</h2>
       <span className={classes.iconRight}>{props.rightIcon}</span>

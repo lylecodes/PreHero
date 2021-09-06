f2c2aaf736f45366ba4c1437a056d64efee5e515
Lyle Jensen <74841752+lylej312@users.noreply.github.com>
2021-09-05 22:39:29 -0400

    implemented state logic in menulist

--- components/create/SelectorMenu/IngredientMenuList.js
+++ components/create/SelectorMenu/IngredientMenuList.js
@@ -1,11 +1,24 @@
+import { Fragment, useState } from "react";
+
+import classes from "./SelectorMenu.module.css";
 import MenuItem from "../OverviewMenu/MenuItem";
 
 const IngredientMenuList = (props) => {
+  const [showList, setShowList] = useState(false);
+
+  const showListHandler = () => {};
+
   const ingredients = props.ingredients.map((item, index) => {
     return <MenuItem name={item.name} />;
   });
 
-  return <ul>{ingredients}</ul>;
+  return (
+    <Fragment>
+      {showList && (
+        <ul className={classes.ingredientMenuList}>{ingredients}</ul>
+      )}
+    </Fragment>
+  );
 };
 
 export default IngredientMenuList;
